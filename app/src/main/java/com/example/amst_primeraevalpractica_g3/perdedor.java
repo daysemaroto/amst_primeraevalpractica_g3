a305854e7d540b21618f0d9099c710194b7a5c95
dmaroto98 <dmaroto@espol.edu.ec>
2020-11-27 14:29:33 -0500

    mostrando respuestas correctas en la pantalla de perdedor

--- app/src/main/java/com/example/amst_primeraevalpractica_g3/perdedor.java
+++ app/src/main/java/com/example/amst_primeraevalpractica_g3/perdedor.java
@@ -6,16 +6,39 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class perdedor extends AppCompatActivity {
 
     private Context context;
+
+    private TextView txt_respondidas;
+    String newString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perdedor);
         context=getApplicationContext();
+        txt_respondidas= (TextView) findViewById(R.id.txt_respondidas);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras!=null)
+        {
+            int j =(int) extras.get("errores");
+            j=5-j;
+            txt_respondidas.setText("Ha respondido "+Integer.toString(j)+" preguntas");
+
+        }
+
+
+
+
+
+
+
     }
 
     public  void salir(View view){
